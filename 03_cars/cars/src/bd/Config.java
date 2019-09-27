bbf79d7bce4a7bc81f8b53503521849ae5067b84
Victor Domingos <editor.arcosonline@gmail.com>
2019-09-27 16:29:04 +0100

    DB config

---
+++ 03_cars/cars/src/bd/Config.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd;
+
+
+public class Config {
+        private static String baseDados = "";
+        private static String servidor = "";
+        private static String user = "";
+        private static String password = "";
+
+    public static String getBaseDados() {
+        return baseDados;
+    }
+
+
+    public static String getServidor() {
+        return servidor;
+    }
+
+
+    public static String getUser() {
+        return user;
+    }
+
+
+    public static String getPassword() {
+        return password;
+    }
+
+}
+
